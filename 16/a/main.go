66b6dac2d93bf6f35f751ab823463de46a41bdbe
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2017-12-27 23:15:32 +0000

    tests pass, chanllenge doesn't

--- 16/a/main.go
+++ 16/a/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main(){
-    
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	alphabet := strings.Split("abcdefghijklmnop", "")
+	// alphabet := strings.Split("abcde", "")
+	actions := getActions("input")
+	d := newDance(alphabet)
+	fmt.Println(d)
+
+	for _, action := range actions {
+
+		actionParts := strings.Split(action, "")
+
+		switch actionParts[0] {
+		case "s":
+			spin, _ := strconv.Atoi(actionParts[1])
+			d.spin(spin)
+			break
+		case "x":
+			a, _ := strconv.Atoi(actionParts[1])
+			b, _ := strconv.Atoi(actionParts[3])
+			d.exchange(a, b)
+			break
+		case "p":
+			d.partner(actionParts[1], actionParts[3])
+			break
+		default:
+			log.Println("Unknown instruction ", actions)
+			break
+		}
+	}
+
+	fmt.Println(d.sequence())
 }
